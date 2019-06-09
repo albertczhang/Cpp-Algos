1c0b9660e504a2b490da44c28809a35b81227759
Albert Zhang <albertczhang@berkeley.edu>
2019-06-09 09:41:55 -0400

    initial commit

---
+++ LinAlgOps/cmake-build-debug/CMakeFiles/LinAlgOps.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LinAlgOps.dir/main.cpp.o"
+  "LinAlgOps.pdb"
+  "LinAlgOps"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LinAlgOps.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ LinAlgOps/cmake-build-debug/CMakeFiles/LinAlgOps.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for LinAlgOps.
+# This may be replaced when dependencies are built.
