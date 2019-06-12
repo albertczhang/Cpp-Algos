2d82e88b1de1bba9cc028d571b4631fdb21ac256
Albert Zhang <albertczhang@berkeley.edu>
2019-06-12 12:24:59 -0400

    Implemented Gaussian Algo to calculate determinant

--- LinAlgOps/Matrix.cpp
+++ LinAlgOps/Matrix.cpp
@@ -61,23 +61,31 @@ T Matrix<T>::get(int i, int j) const {
 template<class T>
 void Matrix<T>::show(int padding) {
     int w = padding;
-    cout << "⎡";
-    for (int j = 0; j < n - 1; j++) {
-        cout << mat[0][j] << setw(w) << setfill(' ');
-    }
-    cout << mat[0][n - 1] << "⎤\n";
-    for (int i = 1; i < m - 1; i++) {
-        cout << "⎢";
+    if (m == 1) {
+        cout << "[";
         for (int j = 0; j < n - 1; j++) {
-            cout << mat[i][j] << setw(w) << setfill(' ');
+            cout << mat[0][j] << setw(w) << setfill(' ');
         }
-        cout << mat[i][n - 1] << "⎥\n";
+        cout << mat[0][n - 1] << "]\n";
+    } else {
+        cout << "⎡";
+        for (int j = 0; j < n - 1; j++) {
+            cout << mat[0][j] << setw(w) << setfill(' ');
+        }
+        cout << mat[0][n - 1] << "⎤\n";
+        for (int i = 1; i < m - 1; i++) {
+            cout << "⎢";
+            for (int j = 0; j < n - 1; j++) {
+                cout << mat[i][j] << setw(w) << setfill(' ');
+            }
+            cout << mat[i][n - 1] << "⎥\n";
+        }
+        cout << "⎣";
+        for (int j = 0; j < n - 1; j++) {
+            cout << mat[m - 1][j] << setw(w) << setfill(' ');
+        }
+        cout << mat[m - 1][n - 1] << "⎦\n";
     }
-    cout << "⎣";
-    for (int j = 0; j < n - 1; j++) {
-        cout << mat[m - 1][j] << setw(w) << setfill(' ');
-    }
-    cout << mat[m - 1][n - 1] << "⎦\n";
 }
 
 
@@ -269,36 +277,94 @@ T Matrix<T>::det(int algo) const {
         exit(0);
     }
     /* TODO : implement determinant function */
-    /* 0: efficient computation of determinant
+    /* 0: Gaussian Elimination (ONLY WORKS WITH DOUBLES/IDOUBLES NOT INTS/IINTS)
      * 1: naive (inefficient) algorithm
      * */
-    switch (algo) {
-        case 0:
-
-            break;
-        case 1:
-            if (m == 1 && n == 1) {
-                return get(1, 1);
-            }
-            T result = 0;
-            for (int j = 1, f = 1; j <= n; j++, f *= -1) {
-                Matrix<T> minor (m - 1, n - 1);
-                for (int i = 2; i <= m; i++) {
-                    for (int k = 1; k < j; k++) {
-                        minor.set(i - 1, k, get(i, k));
-                    }
-                    for (int k = j + 1; k <= n; k++) {
-                        minor.set(i - 1, k - 1, get(i, k));
+    if (algo == 0) {
+        Matrix<T> copy(n, n);
+        copy = *this;
+        T result(1);
+        for (int j = 1; j <= n; j++) {
+            bool flag = false;
+            for (int i = j; i <= m; i++) {
+                T entry = copy.get(i, j);
+                if (entry != 0) {
+                    flag = true;
+                    if (i == j)
+                        break;
+                    else if (i > j) {
+                        copy.rowSwap(i, j);
+                        T flip(-1);
+                        result = result * flip;
+                        break;
                     }
                 }
-                T factor (f);
-                result = result + factor * get(1, j) * minor.det(1);
-//                cout << j << " " << factor << " " << get(1, j) << " " << minor.det(1) << endl;
             }
-            return result;
+            if (!flag) {
+                T d(0);
+                return d;
+            }
+            T factor(copy.get(j, j));
+            result = result * factor;
+            copy.rowDiv(j, factor);
+            if (j + 1 <= m) {
+                for (int i = j + 1; i <= m; i++) {
+                    copy.rowMinus(j, i, copy.get(i, j));
+                }
+            }
+        }
+        return result;
+    } else if (algo == 1) {
+
+        if (m == 1 && n == 1) {
+            return get(1, 1);
+        }
+        T result = 0;
+        for (int j = 1, f = 1; j <= n; j++, f *= -1) {
+            Matrix<T> minor(m - 1, n - 1);
+            for (int i = 2; i <= m; i++) {
+                for (int k = 1; k < j; k++) {
+                    minor.set(i - 1, k, get(i, k));
+                }
+                for (int k = j + 1; k <= n; k++) {
+                    minor.set(i - 1, k - 1, get(i, k));
+                }
+            }
+            T factor(f);
+            result = result + factor * get(1, j) * minor.det(1);
+        }
+        return result;
+    }
+
+}
+
+template<class T>
+void Matrix<T>::rowMinus(int i1, int i2, T factor) {
+    for (int j = 1; j <= n; j++) {
+        set(i2, j, get(i2, j) - get(i1, j) * factor);
     }
 }
 
+template<class T>
+void Matrix<T>::rowSwap(int i1, int i2) {
+    vector<T> temp = mat[i1 - 1];
+    mat[i1 - 1] = mat[i2 - 1];
+    mat[i2 - 1] = temp;
+}
+
+template<class T>
+void Matrix<T>::rowMult(int i, T factor) {
+    for (int j = 1; j <= n; j++) {
+        set(i, j, factor * get(i, j));
+    }
+}
+
+template<class T>
+void Matrix<T>::rowDiv(int i, T factor) {
+    for (int j = 1; j <= n; j++) {
+        set(i, j, get(i, j) / factor);
+    }
+}
 
 template
 class Matrix<int>;
--- LinAlgOps/main.cpp
+++ LinAlgOps/main.cpp
@@ -2,39 +2,68 @@
 #include "Matrix.h"
 #include <math.h>
 #include <iomanip>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
+int randInt(int min, int max) {
+    return rand() % (max - min + 1) + min;
+}
+
+double randDouble(double min, double max) {
+    return (max - min) * ((double) rand() / (double) RAND_MAX) + min;
+}
+
 int main() {
 
-//    cout << setprecision(3);
-//    Matrix<int> M;
 
-//    idouble zarr[4];
-//    zarr[0] = idouble(3, 4);
-//    zarr[1] = idouble(4, 5);
-//    zarr[2] = idouble(5, 6);
-//    zarr[3] = idouble(6, 7);
-//    for (int i = 0; i < 4; i++) {
-//        zarr[i].conj();
-//    }
-//    for (int i = 0; i < 4; i++) {
-//        cout << zarr[i] << endl;
-//    }
+    srand(time(NULL));
 
-    int n = 6;
-    Matrix<double> m1(n, n);
-    for (int i = 1; i <= n; i++) {
-        for (int j = i + 1; j <= n; j++) {
-            m1.set(i, j, i);
-            m1.set(j, i, -i);
+    cout << setprecision(2);
+
+    const int NUM_TRIALS = 1000;
+    int good_count = 0;
+    int rough_count = 0;
+    int bad_count = 0;
+    double error = 0;
+    for (int trials = 0; trials < NUM_TRIALS; trials++) {
+
+        int n = rand() % 7 + 2;
+        Matrix<double> m1(n, n);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                m1.set(i, j, randDouble(-10, 10));
+
+            }
+        }
+        m1.show(6);
+        int d1 = m1.det(1);
+        int d2 = m1.det();
+        if (d1 == d2) {
+            good_count++;
+            cout << "Determinants match: " <<
+                 d1 << " = " << d2 << endl << endl;
+        }
+        else if (d1 - d2 > -2 && d1 - d2 < 2) {
+            rough_count++;
+            error += abs((d1 - d2) / d1 * 100);
+            cout << "Determinants are off by 1: " <<
+                 d1 << " = " << d2 << endl << endl;
+            cout << "error: " << error << endl;
+        } else {
+            bad_count++;
+            error += abs((d1 - d2) / d1 * 100);
+            cout << "Determinants don't match: " <<
+                 d1 << " != " << d2 << endl << endl;
+            cout << "error: " << error << endl;
         }
     }
-    cout << setprecision(2);
-    m1.show(6);
-    cout << m1.det(1) << endl;
-    m1.show();
-
+    error /= NUM_TRIALS;
+    cout << "Matching: " << good_count << endl;
+    cout << "Off by one: " << rough_count << endl;
+    cout << "Not Matching: " << bad_count << endl;
+    cout << "Mean Error: " << error << "%" << endl;
 
     return 0;
 }
